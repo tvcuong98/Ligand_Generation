e9b370e28bf98643ce586e98f8976fbac1adeec3
sonhm <unikool8989@gmail.com>
2024-05-16 07:32:12 +0700

    fixing things , adding more data files

--- process_protein_3d.py
+++ process_protein_3d.py
@@ -6,7 +6,7 @@ import selfies as sf
 import pandas as pd
 import torch
 from utils.protein_utils import featurize_as_graph
-from utils.transform import BaseTransform
+# from utils.transform import BaseTransform
 
 
 parser = argparse.ArgumentParser()
@@ -15,16 +15,18 @@ parser.add_argument("--data", type = str, default = "kiba")
 parser.add_argument("--device", type = str, default = "cuda:0")
 
 args = parser.parse_args()
-transform = BaseTransform(device = args.device)
+# transform = BaseTransform(device = args.device)
 
 if args.data == "davis":
-    root = "/data/davis/prot_3d_for_Davis"
+    root = "./data/davis/prot_3d_for_Davis"
 else:
-    root = "/data/kiba/prot_3d_for_KIBA"
+    root = "./data/kiba/prot_3d_for_KIBA"
 
 folders = os.listdir(root)
 for folder in tqdm(folders): 
     path = os.path.join(root, folder)
     protein_df = fo.bp_to_df(fo.read_pdb(path))
     protein_graph = featurize_as_graph(protein_df)
+    save_path = os.path.join(root,"res_graph")
+    if not os.path.exists(save_path): os.mkdir(save_path)
     torch.save(protein_graph, os.path.join(root, "res_graph", f"{folder}.pt"))
